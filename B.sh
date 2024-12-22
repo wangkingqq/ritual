17c61adbb8e57c38b4ce03e7e67db8b811c3046d
wangkingqq <40823547+wangkingqq@users.noreply.github.com>
2024-12-22 16:03:54 +0800

    Add files via upload

---
+++ B.sh
@@ -0,0 +1,224 @@
+
+#!/bin/bash
+
+sudo apt install python3-pip python3.12-venv -y
+cd ~
+python3 -m venv ./venv
+cd ~/venv/bin
+./pip3 install infernet-cli infernet-client
+sudo ./infernet-cli config anvil --skip
+sudo ./infernet-cli add-service hello-world --skip
+sudo ./infernet-cli start
+cd ~
+# Clone locally
+git clone --recurse-submodules https://github.com/ritual-net/infernet-container-starter
+# Navigate to the repository
+cd infernet-container-starter
+ 
+# cd in, install, and cd out
+cd projects/hello-world/contracts && forge install --no-commit foundry-rs/forge-std && forge install --no-commit ritual-net/infernet-sdk && cd ../../../
+make deploy-contracts project=hello-world
+
+
+
+# 脚本保存路径
+SCRIPT_PATH="$ritual/A.sh"
+
+# 自动设置快捷键的功能
+function check_and_set_alias() {
+    local alias_name="rit"
+    local shell_rc="$HOME/.bashrc"
+
+    # 对于Zsh用户，使用.zshrc
+    if [ -n "$ZSH_VERSION" ]; then
+        shell_rc="$HOME/.zshrc"
+    elif [ -n "$BASH_VERSION" ]; then
+        shell_rc="$HOME/.bashrc"
+    fi
+
+    # 检查快捷键是否已经设置
+    if ! grep -q "$alias_name" "$shell_rc"; then
+        echo "设置快捷键 '$alias_name' 到 $shell_rc"
+        echo "alias $alias_name='bash $SCRIPT_PATH'" >> "$shell_rc"
+        # 添加提醒用户激活快捷键的信息
+        echo "快捷键 '$alias_name' 已设置。请运行 'source $shell_rc' 来激活快捷键，或重新打开终端。"
+    else
+        # 如果快捷键已经设置，提供一个提示信息
+        echo "快捷键 '$alias_name' 已经设置在 $shell_rc。"
+        echo "如果快捷键不起作用，请尝试运行 'source $shell_rc' 或重新打开终端。"
+    fi
+}
+
+# 节点安装功能
+function install_node() {
+cd ~
+
+
+# 更新系统包列表
+sudo apt update
+
+
+# 克隆 ritual-net 仓库
+git clone https://github.com/ritual-net/infernet-node
+
+# 进入 infernet-deploy 目录
+cd infernet-node
+
+# 设置标签
+tag="v1.0.0"
+# 构建镜像
+sudo docker build -t ritualnetwork/infernet-node:$tag .
+
+# 进入目录
+cd deploy
+
+# 使用cat命令将配置写入config.json
+cat > config.json <<EOF
+{
+  "log_path": "infernet_node.log",
+  "manage_containers": true,
+  "server": {
+    "port": 4000,
+    "rate_limit": {
+      "num_requests": 100,
+      "period": 100
+    }
+  },
+  "chain": {
+    "enabled": true,
+    "trail_head_blocks": 5,
+    "rpc_url": "https://api.zan.top/node/v1/base/mainnet/0168bd02545f45a0b70ad85af3ca2af3",
+    "registry_address": "0x3B1554f346DFe5c482Bb4BA31b880c1C18412170",
+    "wallet": {
+      "max_gas_limit": 5000000,
+      "private_key": "cd02a3513b6ec689e831330da6399ac3d35e32a2c09326a7039a004b75321d6f",
+      "payment_address": "0x9bDe142B7F14C7700b03bE9Bc670F5E458A9b290",
+      "allowed_sim_errors": ["not enough balance"]
+    },
+    "snapshot_sync": {
+      "sleep": 1.5,
+      "batch_size": 200
+    }
+  },
+  "docker": {
+    "username": "wolfrainbbq",
+    "password": "Heisha631028"
+  },
+  "redis": {
+    "host": "redis",
+    "port": 6379
+  },
+  "forward_stats": true,
+  "startup_wait": 1.0,
+  "containers": [
+    {
+      "id": "hello-world",
+      "image": "ritualnetwork/hello-world-infernet:latest",
+      "external": true,
+      "port": "3000",
+      "allowed_delegate_addresses": [],
+      "allowed_addresses": [],
+      "allowed_ips": [],
+      "command": "--bind=0.0.0.0:3000 --workers=2",
+      "env": {},
+      "volumes": [],
+      "accepted_payments": {
+        "0x0000000000000000000000000000000000000000": 1000000000000000000,
+        "0x59F2f1fCfE2474fD5F0b9BA1E73ca90b143Eb8d0": 1000000000000000000
+      },
+      "generates_proofs": false
+    }
+  ]
+}
+EOF
+
+echo "Config 文件设置完成"
+
+
+# 安装基本组件
+sudo apt install pkg-config curl build-essential libssl-dev libclang-dev -y
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# 检查 Docker 是否已安装
+if ! command -v docker &> /dev/null
+then
+    # 如果 Docker 未安装，则进行安装
+    echo "未检测到 Docker，正在安装..."
+    sudo apt-get install ca-certificates curl gnupg lsb-release
+
+    # 添加 Docker 官方 GPG 密钥
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    # 设置 Docker 仓库
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # 授权 Docker 文件
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
+
+    # 安装 Docker 最新版本
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+    sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+    docker compose version
+    
+else
+    echo "Docker 已安装。"
+fi
+# 启动容器
+sudo docker compose up -d
+
+echo "=========================安装完成======================================"
+echo "请使用cd infernet-node/deploy 进入目录后，再使用docker compose logs -f 查询日志 "
+
+}
+
+# 查看节点日志
+function check_service_status() {
+    cd infernet-node/deploy
+    docker compose logs -f
+}
+
+
+# 主菜单
+function main_menu() {
+    clear
+    echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
+    echo "================================================================"
+    echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
+    echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
+    echo "请选择要执行的操作:"
+    echo "1. 运行节点"
+    echo "2. 查看节点日志"
+    echo "3. 设置快捷键的功能"
+    read -p "请输入选项（1-3）: " OPTION
+
+    case $OPTION in
+    1) install_node ;;
+    2) check_service_status ;;
+    3) check_and_set_alias ;;  
+    *) echo "无效选项。" ;;
+    esac
+}
+
+# 显示主菜单
+main_menu
